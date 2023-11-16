562e8456cdf76d4278e1793e7cfff98f4d7f4d17
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2023-11-16 12:37:23 -0300

    Create pegar_detalhes_usuario.php

---
+++ ConexaoBDMobile/pegar_detalhes_usuario.php
@@ -0,0 +1,71 @@
+<?php
+require_once('conexao_db.php');
+
+// autenticação
+require_once('autenticacao.php');
+
+// array de resposta
+$resposta = array();
+
+// verifica se o usuário conseguiu autenticar
+if(autenticar($db_con)) {
+
+	// Verifica se o parametro id foi enviado na requisicao
+	if (isset($_GET["id"])) {
+		
+		// Aqui sao obtidos os parametros
+		$id = $_GET['id'];
+		
+		// Obtem do BD os detalhes do produto com id especificado na requisicao GET
+		$consulta = $db_con->prepare("SELECT * FROM usuario WHERE id = $id");
+	
+		if ($consulta->execute()) {
+			if ($consulta->rowCount() > 0) {
+	
+				// Se o produto existe, os dados completos do produto 
+				// sao adicionados no array de resposta. A imagem nao 
+				// e entregue agora pois ha um php exclusivo para obter 
+				// a imagem do produto.
+				$linha = $consulta->fetch(PDO::FETCH_ASSOC);
+				$resposta["nome"] = $linha["nome"];
+				$resposta["foto"] = $linha["foto"];
+        $resposta["data_nascimento"] = $linha["data_nascimento"];
+
+				// Caso o usuario exista no BD, o cliente 
+				// recebe a chave "sucesso" com valor 1.
+				$resposta["sucesso"] = 1;
+				
+			} else {
+				// Caso o produto nao exista no BD, o cliente 
+				// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+				// motivo da falha.
+				$resposta["sucesso"] = 0;
+				$resposta["erro"] = "Usuario não encontrado";
+			}
+		} else {
+			// Caso ocorra falha no BD, o cliente 
+			// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+			// motivo da falha.
+			$resposta["sucesso"] = 0;
+			$resposta["erro"] = "Erro no BD: " . $consulta->$error;
+		}
+	} else {
+		// Se a requisicao foi feita incorretamente, ou seja, os parametros 
+		// nao foram enviados corretamente para o servidor, o cliente 
+		// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+		// motivo da falha.
+		$resposta["sucesso"] = 0;
+		$resposta["erro"] = "Campo requerido não preenchido";
+	}
+}
+else {
+	// senha ou usuario nao confere
+	$resposta["sucesso"] = 0;
+	$resposta["error"] = "usuario ou senha não confere";
+}
+// Fecha a conexao com o BD
+$db_con = null;
+
+// Converte a resposta para o formato JSON.
+echo json_encode($resposta);
+?>

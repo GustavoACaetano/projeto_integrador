cf82b42149ca7e51705b42ce61c77a4a4c4fdf49
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2023-11-28 07:41:15 -0300

    Update atualizar_dados_usuario.php

--- ConexaoBDMobile/atualizar_dados_usuario.php
+++ ConexaoBDMobile/atualizar_dados_usuario.php
@@ -5,7 +5,7 @@ require_once('conexao_db.php');
 $resposta = array();
 
 // Verifica se o parametro id foi enviado na requisicao
-if (isset($_GET["id"]) && isset($_GET["nome"]) && isset($_GET["email"]) && isset($_GET["data"]) && isset($_GET["telefone"]) && isset($_get["caminho_foto"])) {
+if (isset($_GET["id"]) && isset($_GET["nome"]) && isset($_GET["email"]) && isset($_GET["data"]) && isset($_GET["telefone"])) {
 	
 	// Aqui sao obtidos os parametros
 	$id = $_GET['id'];
@@ -14,11 +14,49 @@ if (isset($_GET["id"]) && isset($_GET["nome"]) && isset($_GET["email"]) && isset
 	$data = trim($_GET["data"]);
 	$telefone = trim($_GET["telefone"]);
 	$caminho_foto = trim($_GET["caminho_foto"]);
-	// Ver a parada da foto com o gustavo e daniel
-	
-	// Obtem do BD os detalhes do produto com id especificado na requisicao GET
-	$consulta = $db_con->prepare("UPDATE usuario set nome = " . $nome . ", email = " . $email . ", data = " . $data . ", telefone = " . $telefone . " WHERE id = " . $id);
+	if ($_FILES["caminho_foto"]["size"] > 0){
+	        $client_id = "6d2b5be8400b2b3";
+	        $filename = $_FILES['foto']['tmp_name'];
+	        
+	        $image_data = file_get_contents($filename);
+	        $image_data_base64 = base64_encode($image_data);
+	        
+	        $api_url = 'https://api.imgur.com/3/image.json';
+	        
+	        $headers = [
+	            'Authorization: Client-ID ' . $client_id,
+	            'Content-Type: application/x-www-form-urlencoded'
+	        ];
+	        
+	        $postData = http_build_query(['image' => $image_data_base64]);
+	        
+	        $options = [
+	            'http' => [
+	                'header' => implode("\r\n", $headers),
+	                'method' => 'POST',
+	                'content' => $postData
+	            ]
+	        ];
+	        
+	        $context = stream_context_create($options);
+	        $result = file_get_contents($api_url, false, $context);
+	        
+	        if ($result === FALSE) {
+	        	echo "Erro ao enviar arquivo para o Imgur";
+	        } else {
+	        	$response = json_decode($result, true);
+	        	$foto = $response['data']['link'];
+	        }
 
+	        $consulta = $db_con->prepare("UPDATE usuario set nome='$nome', telefone='$telefone', data_nascimento='$data_nascimento', fk_intuito_id='$intuito', foto='$foto' WHERE email='$email'");
+	        $resposta["sucesso"] = 1;
+	        $consulta->execute();
+	} else {
+		$consulta = $db_con->prepare("UPDATE usuario set nome='$nome', telefone='$telefone', data_nascimento='$data_nascimento', fk_intuito_id='$intuito' WHERE email='$email'");
+		$resposta["sucesso"] = 1;
+		$consulta->execute();
+	}
+	
 	if ($consulta->execute()) {
 		$resposta["sucesso"] = 1;
 	} else {

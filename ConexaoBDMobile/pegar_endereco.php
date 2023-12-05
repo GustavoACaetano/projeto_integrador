e369c2d193742ca1aefd05cc3515a1780c828b86
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2023-12-04 21:08:58 -0300

    Update pegar_endereco.php

--- ConexaoBDMobile/pegar_endereco.php
+++ ConexaoBDMobile/pegar_endereco.php
@@ -1,7 +1,5 @@
 <?php
-require_once('conexao_db.php');
-
-function pegarEndereco($endereco){
+function pegarEndereco($db_con, $endereco){
   $consulta_endereco = $db_con->prepare("SELECT * FROM endereco WHERE id = " . $endereco);
   $consulta_endereco->execute();
   $linha_endereco = $consulta_endereco->fetch(PDO::FETCH_ASSOC);

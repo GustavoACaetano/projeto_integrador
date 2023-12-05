5bc0950529af9544a0cbb530fcb051b57e934a1c
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2023-12-04 21:09:21 -0300

    Update pegar_publico_alvo.php

--- ConexaoBDMobile/pegar_publico_alvo.php
+++ ConexaoBDMobile/pegar_publico_alvo.php
@@ -1,7 +1,5 @@
 <?php
-require_once("conexao_db.php");
-
-function pegarPublicoAlvo($idade_publico){
+function pegarPublicoAlvo($db_con, $idade_publico){
   $consulta_idade_publico = $db_con->prepare("SELECT intervalo FROM idade_publico WHERE id = " . $idade_publico);
   $consulta_idade_publico->execute();
   $linha_idade_publico = $consulta_idade_publico->fetch(PDO::FETCH_ASSOC);

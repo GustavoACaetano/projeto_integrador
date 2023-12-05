813b2dd9df53812d73717fb085b7a0c276d32d59
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2023-12-05 08:54:16 -0300

    Update pegar_eventos_criados.php

--- ConexaoBDMobile/pegar_eventos_criados.php
+++ ConexaoBDMobile/pegar_eventos_criados.php
@@ -8,28 +8,36 @@ require_once('conexao_db.php');
 $resposta = array();
 $resposta["eventos"] = array();
 $email = $_GET["logado"];
-
-$consultaEmail = $db_con->prepare("SELECT * FROM usuario WHERE email='$email'");
-$consultaEmail->execute();
-$linhaUsuario = $consultaEmail->fetch(PDO::FETCH_ASSOC);
-$id_logado = intval($linhaUsuario['id']);
-
-$consulta = $db_con->prepare("SELECT * FROM evento WHERE fk_usuario_id =$id_logado");
-$consulta->execute();
-if ($consulta->rowCount() > 0) {
-    while($linha = $consulta->fetch(PDO::FETCH_ASSOC)){
-        $evento = array();
-        $evento["id"] = $linha['id'];
-        
-        $evento["nome"] = $linha["nome"];
-        $evento["preco"] = number_format($linha["preco"], 2, ',', '');
-        $evento["data"] = $linha["data"];
-        $evento["horario_fim"] = $linha["horario_fim"];
-        $evento["horario_inicio"] = $linha["horario_inicio"];
-        $evento["foto"] = $linha["foto"];
-        // Adiciona o evento no array de eventos.
-        array_push($resposta["eventos"], $evento);
+if(isset($_GET["limit"]) && isset($_GET["offset"])){
+    $consultaEmail = $db_con->prepare("SELECT * FROM usuario WHERE email='$email'");
+    $consultaEmail->execute();
+    $linhaUsuario = $consultaEmail->fetch(PDO::FETCH_ASSOC);
+    $id_logado = intval($linhaUsuario['id']);
+    
+    $consulta = $db_con->prepare("SELECT * FROM evento WHERE fk_usuario_id =$id_logado LIMIT " . $_GET["limit"] . " OFFSET " . $_GET["offset"]);
+    $consulta->execute();
+    if ($consulta->rowCount() > 0) {
+        while($linha = $consulta->fetch(PDO::FETCH_ASSOC)){
+            $evento = array();
+            $evento["id"] = $linha['id'];
+            
+            $evento["nome"] = $linha["nome"];
+            $evento["preco"] = number_format($linha["preco"], 2, ',', '');
+            $evento["data"] = $linha["data"];
+            $evento["horario_fim"] = $linha["horario_fim"];
+            $evento["horario_inicio"] = $linha["horario_inicio"];
+            $evento["foto"] = $linha["foto"];
+            // Adiciona o evento no array de eventos.
+            array_push($resposta["eventos"], $evento);
+        }
     }
+}else {
+	// Se a requisicao foi feita incorretamente, ou seja, os parametros 
+	// nao foram enviados corretamente para o servidor, o cliente 
+	// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+	// motivo da falha.
+	$resposta["sucesso"] = 0;
+	$resposta["erro"] = "Campo requerido não preenchido";
 }
 return json_encode($resposta);
 $db_con = null;

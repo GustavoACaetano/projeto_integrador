40f55a8df279d796877641facb7c4e6fdd5243a0
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2023-12-04 21:07:54 -0300

    Update pegar_intuito.php

--- ConexaoBDMobile/pegar_intuito.php
+++ ConexaoBDMobile/pegar_intuito.php
@@ -1,6 +1,5 @@
 <?php
-require_once('conexao_db.php');
-function pegarIntuito($intuito){
+function pegarIntuito($db_con, $intuito){
   $consulta_intuito = $db_con->prepare("SELECT nome FROM intuito WHERE id = " . $intuito);
   $consulta_intuito->execute();
   $linha_intuito = $consulta_intuito->fetch(PDO::FETCH_ASSOC);

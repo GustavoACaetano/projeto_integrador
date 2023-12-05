8b2f1dea68c0db99b66d38c104722b781598ee8e
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2023-12-04 21:08:07 -0300

    Update pegar_classificacao.php

--- ConexaoBDMobile/pegar_classificacao.php
+++ ConexaoBDMobile/pegar_classificacao.php
@@ -1,7 +1,5 @@
 <?php
-require_once('conexao_db.php');
-
-function pegarClassificacao($classificacao){
+function pegarClassificacao($db_con, $classificacao){
   $consulta_classificacao = $db_con->prepare("SELECT nome FROM classificacao WHERE id = " . $classificacao);
   $consulta_classificacao->execute();
   $linha_classificacao = $consulta_classificacao->fetch(PDO::FETCH_ASSOC);

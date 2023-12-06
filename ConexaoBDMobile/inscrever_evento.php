a7bc3cb812cf3bda81d22644bd55e10778ea467d
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2023-12-06 20:20:59 +0000

    Dialog

--- ConexaoBDMobile/inscrever_evento.php
+++ ConexaoBDMobile/inscrever_evento.php
@@ -9,24 +9,6 @@ if (isset($_POST["email"]) && isset($_POST["id_evento"])) {
 	
 	$email = trim($_POST["email"]);
     $id_evento = $_POST["id_evento"];
-
-	$filename = $_FILES['img']['tmp_name'];
-	$client_id="ce5d3a656e2aa51";
-	$handle = fopen($filename, "r");
-	$data = fread($handle, filesize($filename));
-	$pvars = array('image' => base64_encode($data));
-	$timeout = 30;
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/image.json');
-	curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Client-ID ' . $client_id));
-	curl_setopt($curl, CURLOPT_POST, 1);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $pvars);
-	$out = curl_exec($curl);
-	curl_close ($curl);
-	$pms = json_decode($out,true);
-	$img_url=$pms['data']['link'];
 	
 	$consulta_usuario = $db_con->prepare("SELECT * from usuario where email = '$email'");
     $consulta_usuario->execute();

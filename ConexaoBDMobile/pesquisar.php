070c2b992b82537e6da1839946bf978e34c8a1be
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2023-12-08 18:14:38 -0300

    Update pesquisar.php

--- ConexaoBDMobile/pesquisar.php
+++ ConexaoBDMobile/pesquisar.php
@@ -7,7 +7,7 @@ if(isset($_GET["pesquisa"]) && isset($_GET["limit"]) && isset($_GET["offset"])){
   $limit = $_GET["limit"];
   $offset = $_GET["offset"];
   $pesquisa = $_GET["pesquisa"];
-  $consulta_pesquisa = $db_con->prepare("SELECT * FROM evento WHERE nome LIKE '%$pesquisa%' LIMIT '$limit' OFFSET '$offset'");
+  $consulta_pesquisa = $db_con->prepare("SELECT * FROM evento WHERE nome ILIKE '%$pesquisa%' LIMIT '$limit' OFFSET '$offset'");
   if($consulta_pesquisa->execute()){
       while($linha = $consulta_pesquisa->fetch(PDO::FETCH_ASSOC)){
         $evento = array();
